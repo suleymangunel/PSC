895a1b7dad6b07fb57c2924ac0e459eabf44dc33
Süleyman GÜNEL <39226397+suleymangunel@users.noreply.github.com>
2022-09-17 03:44:12 +0300

    Add files via upload

---
+++ PDNA.h
@@ -0,0 +1,40 @@
+// ============================================================================
+//
+// NAME:    PDNA.h
+//
+// DESCRIPTION:
+//
+//      Single include file to include needed PowerDNA DAQLib includes.
+//
+// ----------------------------------------------------------------------------
+//
+//      Copyright (C) 1997 - 2006 United Electronic Industries, Inc.
+//      All rights reserved.
+//      United Electronic Industries Confidential Information.
+//
+//=============================================================================
+
+#ifndef _PDNA_H_
+#define _PDNA_H_
+
+#ifdef _MSC_VER
+#pragma warning (disable:4200)
+#endif
+
+#include "platformdefs.h"
+#include "fw2win.h"
+#include "powerdna.h"
+#include "UeiDaqPAL.h"
+#include "DAQLib.h"
+#include "DAQLibACB.h"
+#include "DAQLibDMap.h"
+#include "DAQLibMSG.h"
+#include "DAQLibHL.h"
+#include "PDNALibVer.h"
+#include "DAQLibEmb.h"
+#include "DAQLibRTDMAP.h"
+#include "DAQLibRTAsync.h"
+#include "DaqLibHLCustom.h"
+#include "DaqLibCvtData.h"
+
+#endif
